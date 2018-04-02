0431be031dfcf0710df4080a25899811624fa26b
Angel-kou <1844646307@qq.com>
2018-04-02 13:48:06 +0800

    modify caculator by kmj 2018/4/2

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class Calculator {
+    public static final double RATE_FOR_PRICE_ABOVE_TEN_AND_BELOW_HUNDRED = 0.8d;
+    public static final double RATE_FOR_PRICE_ABOVE_HUNDRED = 0.75d;
+    public static final double ACTUAL_PRICE_BELOW_TEN = 0d;
     public double calculate(String[] inputs) {
-       return 0d;
+
+        double totalPrice = Arrays.asList(inputs).stream()
+                .map((goods) -> {
+                    double goodsPrice = Double.parseDouble(goods.replaceAll("[^\\d.]", ""));
+                    return calculateActualPrice(goodsPrice);})
+                .reduce((sum, cost) -> sum + cost)
+                .get();
+        return roundToTwoDecimal(totalPrice);
+    }
+
+    private Double calculateActualPrice(double goodsPrice) {
+        if (goodsPrice <= 10d) {
+            return ACTUAL_PRICE_BELOW_TEN;
+        } else if (goodsPrice > 10d && goodsPrice <= 100d) {
+            return goodsPrice * RATE_FOR_PRICE_ABOVE_TEN_AND_BELOW_HUNDRED;
+        } else {
+            return goodsPrice * RATE_FOR_PRICE_ABOVE_HUNDRED;
+        }
+    }
+
+
+    private double roundToTwoDecimal(double number) {
+        return (double) Math.round(number * 100) / 100;
     }
 }
