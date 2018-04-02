c7b18597343ea17cba4e8c96479c5fa6c8a698ba
Angel-kou <1844646307@qq.com>
2018-04-02 13:35:05 +0800

    modiy calculator method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Calculator {
     public String[] toUniqueUppercase(String[] fruits) {
-        return null;
+
+        return Arrays.asList(fruits).stream().map((fruit) -> fruit.toUpperCase()).distinct().toArray(String[]::new);
+
     }
 }
